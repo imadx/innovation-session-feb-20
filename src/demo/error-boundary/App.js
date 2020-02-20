190f6cc3131a48f362f9e470c9c08103ee84616e
Ishan Madhusanka <ahtimadhusanka@gmail.com>
2020-02-20 11:51:27 +0530

    update error boundary styles

--- src/demo/error-boundary/App.js
+++ src/demo/error-boundary/App.js
@@ -2,17 +2,26 @@ import React from 'react';
 import CallError from './CallError';
 import ErrorBoundary from './ErrorBoundary';
 
+import logo from '../../img/logo.svg';
+
 const App = () => {
-  return (
-    <div className='App'>
-        <h1>
-            Demo - Error Boundary
-        </h1>
-        <ErrorBoundary>
-            <CallError />
-        </ErrorBoundary>
-    </div>
-  );
+	return (
+		<div className='App App-home'>
+			<div className='content'>
+				<hgroup>
+					<h1>Error Boundary</h1>
+					<h3>Hint: Click the button until the counter reaches 5</h3>
+				</hgroup>
+
+				<ErrorBoundary>
+					<CallError />
+				</ErrorBoundary>
+			</div>
+			<div className='footer'>
+				<img src={logo} alt='' />
+			</div>
+		</div>
+	);
 };
 
 export default App;
