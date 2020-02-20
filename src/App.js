5dec30bdc5b71f3ba38be9b7256a797aa8c62a43
Ishan Madhusanka <ahtimadhusanka@gmail.com>
2020-02-20 10:15:29 +0530

    initial commit with a router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { Suspense, lazy } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Home = lazy(() => import('./demo/index'));
+const DemoMemoize = lazy(() => import('./demo/memoize/App'));
+const DemoContext = lazy(() => import('./demo/context-api/App'));
+
+const App = () => (
+  <Router>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/memoize" component={DemoMemoize}/>
+        <Route path="/context-api" component={DemoContext}/>
+      </Switch>
+    </Suspense>
+  </Router>
+);
 
 export default App;
